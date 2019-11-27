b69b11f4d8df4234a746935edbda808a04310c3d
Ivan Herman <ivan@ivan-herman.net>
2019-11-27 11:32:38 +0100

    errata is es6, jQuery and underline free

--- assets/errata.js
+++ assets/errata.js
@@ -13,109 +13,161 @@ a new comment on the issue may be added beginning with the word "Summary".
 As for the report, the structure of the HTML file can be seen in the index.html file. The net result is
 that the active issues are displayed in different sections, depending on the presence of specific labels. The relevant values are set
 through some data-* attributes on the elements.
-
 */
 
-$(document).ready(() => {
-    const display_issue = (node, issue, comments, labels) => {
-        const display_labels = labels.filter((label) => label !== 'Errata').join(', ');
-        const div = document.createElement('div');
-        div.className = 'issue';
-        node.append(div);
-        div.innerHTML = `<h3>"${issue.title}"</h3>`;
-
-        let state = 'Open';
-        let state_class = 'state_open';
-        if (issue.state !== 'open') {
-            state = 'Closed';
-            state_class = 'state_closed';
+/**
+ * Fetch a JSON file.
+ * 
+ * @async
+ * @param {string} resource_url 
+ */
+async function fetch_json(resource_url) {
+    // If there is a problem, an exception is raised
+    return new Promise((resolve, reject) => {
+        try {
+            // This is a real URL, whose content must be accessed via HTTP(S)
+            // An exception is raised if the URL has security/sanity issues.
+            fetch(resource_url)
+                .then((response) => {
+                    return response.text();
+                })
+                .then((body) => {
+                    resolve(JSON.parse(body));
+                })
+        } catch (err) {
+            reject(err);
         }
+    });
+};
 
-        const pull_request = (issue.pull_request === undefined) ? "No" : "Yes";
-        const p1 = document.createElement('p');
-        p1.innerHTML = `<span class='what'>Issue number:</span> <a href='${issue.html_url}'>#${issue.number}</a><br>`  +
-                       `<span class='what'>Raised by:</span> <a href='${issue.user.url}'>@${issue.user.login}</a><br>` +
-                       `<span class='what'>Extra labels:</span> ${display_labels}<br>`                                 +
-                       `<span class='what'>Pull request? </span> ${pull_request}<br>`                                  +
-                       `<span class='what'>Status:</span> <span class="${state_class}">${state}</span><br> `           ;
-        div.append(p1);
+/**
+ * Display a single issue with all its comments. The array of comments is used to look for a 'Summary' note; if found, added to the issue display.
+ * 
+ * @param {HTMLElement} node - place to place all the issue description, usually a section element
+ * @param {object} issue - issue object as return by the Github API
+ * @param {object[]} comments - array of comments as returned by the Github API 
+ * @param {string[]} labels - labels assigned to the issue 
+ */
+function display_issue(node, issue, comments, labels) {
+    const display_labels = labels.filter((label) => label !== 'Errata').join(', ');
+    const div = document.createElement('div');
+    div.className = 'issue';
+    node.append(div);
+    div.innerHTML = `<h3>"${issue.title}"</h3>`;
 
-        const p2 = document.createElement('p');
-        p2.innerHTML = `<span class='what'><a href='${issue.html_url}'>Initial description:</a></span> ${issue.body}`;
-        div.append(p2);
-
-        // See if a summary has been added to the comment.
-        let summary = undefined;
-        comments.forEach((comment) => {
-            if( comment.body.search('^Summary:') !== -1 ) {
-                summary = comment;
-            }
-        });
-
-        if( summary !== undefined ) {
-            // @ts-ignore
-            const summary_text = summary.body.substr('Summary:'.length)
-            // @ts-ignore
-            const p3 = document.createElement('p');
-            p3.innerHTML = `<span class='what'><a href='${summary.html_url}'>Erratum summary:</a></span> ${summary_text}`;
-            div.append(p3);
-        }
+    let state = 'Open';
+    let state_class = 'state_open';
+    if (issue.state !== 'open') {
+        state = 'Closed';
+        state_class = 'state_closed';
     }
 
-    const render_issue = (issue, comments) => {
-        const labels = issue.labels.map((obj) => obj.name);
-        const get_subsection = (node, lbls) => lbls.includes('Editorial') ? node.querySelector('section:last-of-type') : node.querySelector('section:first-of-type');
+    const pull_request = (issue.pull_request === undefined) ? "No" : "Yes";
+    const p1 = document.createElement('p');
+    p1.innerHTML = `<span class='what'>Issue number:</span> <a href='${issue.html_url}'>#${issue.number}</a><br>`  +
+                    `<span class='what'>Raised by:</span> <a href='${issue.user.url}'>@${issue.user.login}</a><br>` +
+                    `<span class='what'>Extra labels:</span> ${display_labels}<br>`                                 +
+                    `<span class='what'>Pull request? </span> ${pull_request}<br>`                                  +
+                    `<span class='what'>Status:</span> <span class="${state_class}">${state}</span><br> `           ;
+    div.append(p1);
 
-        let displayed = false;
-        const sections = document.querySelectorAll('main > section');
+    const p2 = document.createElement('p');
+    p2.innerHTML = `<span class='what'><a href='${issue.html_url}'>Initial description:</a></span> ${issue.body}`;
+    div.append(p2);
 
-        // first looking for sections corresponding to the main errata labels
+    // See if a summary has been added to the comment.
+    let summary = undefined;
+    comments.forEach((comment) => {
+        if( comment.body.search('^Summary:') !== -1 ) {
+            summary = comment;
+        }
+    });
+
+    if( summary !== undefined ) {
+        // @ts-ignore
+        const summary_text = summary.body.substr('Summary:'.length)
+        // @ts-ignore
+        const p3 = document.createElement('p');
+        p3.innerHTML = `<span class='what'><a href='${summary.html_url}'>Erratum summary:</a></span> ${summary_text}`;
+        div.append(p3);
+    }
+}
+
+/**
+ * Find the right section in the HTML content to display an issue. This involves
+ * 
+ * - Find the section corresponding to a document via a match of a label in the HTML section element; if not found, display the issue in the 'nolabel' section
+ * - Find, within the target, the subsection corresponding to an Editorial issue (if so marked) or the substantial one.
+ * 
+ * @param {object} issue - the issue structure as returned by the Github API
+ * @param {object[]} comments - array of comment objects, as returned by the Github API
+ */
+function render_issue(issue, comments) {
+    // Get the labels as an array of strings
+    const labels = issue.labels.map((obj) => obj.name);
+    const get_subsection = (node, lbls) => lbls.includes('Editorial') ? node.querySelector('section:last-of-type') : node.querySelector('section:first-of-type');
+
+    let displayed = false;
+    const sections = document.querySelectorAll('main > section');
+
+    // first looking for sections corresponding to the main errata labels
+    sections.forEach((section) => {
+        if (labels.includes(section.dataset.erratalabel)) {
+            const subsection = get_subsection(section, labels);
+            display_issue(subsection, issue, comments, labels)
+            displayed = true;
+        }
+    });
+
+    // As a fallback, look for the section marked for 'nolabel' entries.
+    if (!displayed) {
+        // looking for the 'nolabel' data attribute
         sections.forEach((section) => {
-            if (labels.includes(section.dataset.erratalabel)) {
+            if (section.dataset.nolabel !== undefined) {
                 const subsection = get_subsection(section, labels);
                 display_issue(subsection, issue, comments, labels)
                 displayed = true;
             }
         });
-
-        if (!displayed) {
-            // looking for the 'nolabel' data attribute
-            sections.forEach((section) => {
-                if (section.dataset.nolabel !== undefined) {
-                    const subsection = get_subsection(section, labels);
-                    display_issue(subsection, issue, comments, labels)
-                    displayed = true;
-                }
-            });
-        }
     }
+}
 
-    // @@@ TODO: get over the fetch thing for getting a JSON, and use this
-    // For this, it may be cleaner to get the two functions above into a separate function and what is below becomes then an
-    // async function to be invoked separately... But then again, maybe not, just have an overall function instead of the top level thingy...
-
-    // Also: the json return may not always be in order. Maybe sorting these by time makes sense!
-
+/**
+ * Process all the issues:
+ * 
+ * - extract the errata issues from the github repo
+ * - extract the number of issues as well as the newest date (to be displayed in the document)
+ * - get the comments for each issue (to be used to look for a possible summary)
+ * - render each issue individually (see `render_issue` above)
+ * 
+ * @async
+ */
+async function process_issues() {
     const dataset = document.getElementsByTagName('head')[0].dataset;
     if (dataset.githubrepo !== undefined) {
         const url_api    = `https://api.github.com/repos/${dataset.githubrepo}/issues?state=all&labels=Errata`;
-        const url_issues = `https://github.com/${dataset.githubrepo}/labels/Errata`;
-        $.getJSON(url_api, (allIssues) => {
-            if( allIssues.length > 0 ) {
-                const latest_change = moment.max(allIssues.map((item) => {
-                    return moment(item.updated_at)
-                }));
-                $('span#date').append(latest_change.format('dddd, MMMM Do YYYY'));
-            } else {
-                $('span#date').append('N/A');
-            }
-            $('span#number').append(allIssues.length);
-            $('span#errata_link').append(`<a href='${url_issues}'>${url_issues}</a>`);
-            $.each(allIssues, (i, issue) => {
-                $.getJSON(issue.comments_url, (comments) => {
-                    render_issue(issue, comments);
-                });
-            });
-        });
+        const url_issues = `https://github.com/${dataset.githubrepo}/issues?q=label%3AErrata`;
+
+        const all_issues = await fetch_json(url_api);        
+        const date_element = document.querySelector('span#date');
+        if( all_issues.length > 0 ) {
+            const latest_change = moment.max(all_issues.map((item) => {
+                return moment(item.updated_at)
+            }));
+            date_element.textContent = latest_change.format('dddd, MMMM Do YYYY');
+        } else {
+            date_element.textContent = 'n/a';
+        }
+        document.querySelector('span#number').textContent = all_issues.length;
+        document.querySelector('span#errata_link').innerHTML = `<a href='${url_issues}'>${url_issues}</a>`;
+
+        const comments_promises = all_issues.map((issue) => fetch_json(issue.comments_url));
+        const all_comments = await Promise.all(comments_promises);
+        let i = 0;
+        for (; i < all_issues.length; i++) {
+            render_issue(all_issues[i], all_comments[i])
+        }
     }
-});
+}
+
+window.addEventListener('load', process_issues);
